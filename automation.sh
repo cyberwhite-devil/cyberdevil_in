43822063ff2116cef4d005de8ea8549a05a8d509
cyberwhite-devil <116003090+cyberwhite-devil@users.noreply.github.com>
2022-11-09 11:52:44 +0000

    Update automation.sh

--- automation.sh
+++ automation.sh
@@ -1,2 +1,14 @@
 #!/bin/sh
-
+sudo apt-get update
+sudo apt-get upgrade
+sudo apt-get install git
+sudo apt-get install unzip
+sudo apt-get install zip
+sudo apt-get install nano
+sudo apt-get install openjre-11-jdk
+sudo add-apt-repository ppa:wireshark-dev/stable
+sudo apt update
+sudo apt install wireshark
+sudo add-apt-repository ppa:micahflee/ppa
+sudo apt install torbrowser-launcher
+torbrowser-launcher
